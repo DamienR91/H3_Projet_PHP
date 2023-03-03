8d0d95c037ca84b6d9686a41fe97d7e9d6caadd4
H3Airness <damienraunier@gmail.com>
2023-03-03 09:22:19 +0100

    v1.0

---
+++ PHP/_inc/nav.php
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/contact.php">Contact</a>
+    <a href="register.php">Register</a>
+
+</nav>
---
+++ PHP/index.php
@@ -0,0 +1,16 @@
+<?php
+    //inclusions
+    require_once '_inc/header.php';
+    require_once '_inc/nav.php';
+?>
+
+    <h1>Home</h1>
+
+    <p>
+        <a href="/products.php?id=1&message=ok">Afficher un produit</a>
+    </p>
+
+<?php
+    //inclusions
+    require_once '_inc/footer.php';
+?>
